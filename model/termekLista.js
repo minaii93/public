8daa8bd5d8464910e492c6226cbb4762da9b6f35
minaii93 <hajnalkaminaei@gmail.com>
2024-10-05 08:37:37 +0200

    .

--- model/termekLista.js
+++ model/termekLista.js
@@ -1,26 +1,38 @@
 export const termekLista = [
     {
         elnevezes: "Cappuccino",
-        kep: '/img/1.jpg',
+        kep: 'img/1.jpg',
         leiras : "Klasszikus olasz kávé tejhabbal",
         ar: 4
     },
     {
         elnevezes: "Latte Macchiato",
-        kep: '/img/2.jpg',
+        kep: 'img/2.jpg',
         leiras : "Krémes tejjel készült eszpresszó",
         ar: 4
     },
     {
         elnevezes: "Americano",
-        kep: '/img/3.jpg',
+        kep: 'img/3.jpg',
         leiras : "Vízzel készült eszpresszó",
         ar: 4
     },
     {
         elnevezes: "Flat White",
-        kep: '/img/4.jpg',
+        kep: 'img/4.jpg',
         leiras : "Dupla eszpresszó tejhabbal",
         ar: 5
+    },
+    {
+        elnevezes: "Mocha",
+        kep: 'img/5.jpg',
+        leiras: "Csokoládés kávé tejhabbal",
+        ar: 5
+    },
+    {
+        elnevezes: "Espresso",
+        kep: 'img/6.jpg',
+        leiras: "Erős feketekávé",
+        ar: 3
     }
 ]
